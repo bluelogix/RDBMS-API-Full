af2855a67ee90c4cdc594492675211b03bf029a3
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-02-27 19:36:15 -0500

    get id with students finished

--- routers/cohort-router.js
+++ routers/cohort-router.js
@@ -29,13 +29,14 @@ const db = knex(knexConfig.development)
     })
   })
 
-  //GET STUDENTS
+  //GET ID with STUDENTS
       router.get('/:id/students', (req, res) => {
-        db('cohorts')
-        .where({ id: req.params.id })
-        .then(cohort => {
-           if (cohort) {
-            res.status(200).json(cohort);
+        const id = req.params.id
+        db('students')
+        .where({ 'cohort_id': id })
+        .then(student => {
+           if (student.length > 0) {
+            res.status(200).json(student);
           } else {
             res.status(404).json({ message: 'The id cannot be found' });
           }
